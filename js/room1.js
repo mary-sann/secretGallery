f26f8ac49fec8c48e59fadd7413581607ff9e81a
mary-sann <218979812+mary-sann@users.noreply.github.com>
2026-02-21 14:58:42 +0900

    Update room1.js

--- js/room1.js
+++ js/room1.js
@@ -1,45 +1,53 @@
+//キャンバス
 let geom;
 let myshader;
 let ready = false;
-const memo = {x:0.78006,y:-1.4114,z:0.82117};
-let memoGUI;
+let wrapper;
+//カメラ
 let cam;
 const camEye = {x:0,y:-150,z:550};
 const camLook = {x:0,y:-150,z:0};
-let camGuide;
 const config = {
   pan : 0,
   tilt : 0,
-  moveX : 0,
-  moveY : 0,
-  moveZ : 0,
-  dist : 0,
   PAN_MIN : -0.9,
   PAN_MAX :  0.9,
   TILT_MIN : -0.5,
   TILT_MAX :  0.5,
-  MOVE_X_MIN :  -100,
-  MOVE_X_MAX :  100,
-  MOVE_Y_MIN :  -50,
-  MOVE_Y_MAX :  50,
-  MOVE_Z_MIN :  -300,
-  MOVE_Z_MAX :  200
+  sens : 0.5
 };
+const boundingBox = {
+	X_MIN : -100,
+	X_MAX : 100,
+	Y_MIN : -200,
+	Y_MAX : -100,
+	Z_MIN : 100,
+	Z_MAX : 700
+};
+let bBoxShape;
+let isBoundingBox = false;
+//デバックカメラ
 let debugCam;
 let isDebugCam = false;
 const param = {
-        lookAt_TB: 0.0,
+    lookAt_TB: 0.0,
 		lookAt_RL: 0.0,
 		pos_FB: 0.0,
 		pos_RL: 0.0,
-        explode: function () {
-          alert('◼︎カメラのせつめい\n下のスライダーを操作してカメラの向きを変えたり移動できます。\n\n△▽:視線上下\n◁▷:視線左右\n↑↓:移動前後\n←→:移動左右\n');
-        },
+    explode: function () {
+      alert('◼︎カメラのせつめい\n下のスライダーを操作してカメラの向きを変えたり移動できます。\n\n△▽:視線上下\n◁▷:視線左右\n↑↓:移動前後\n←→:移動左右\n');
+    },
 		reset: () => {}
   };
-let wrapper;
+//gui
+const memo = {x:0.78006,y:-1.4114,z:0.82117};
+let memoGUI;
+let camGuide;
 
 document.oncontextmenu = (e) => { e.preventDefault(); }
+//document.addEventListener('focusin', e => {
+//  console.log('focusin →', e.target.tagName);
+//});
 
 async function setup() {
   //canvas設定
@@ -64,7 +72,7 @@ async function setup() {
   
   //3dモデル
   //console.log(THREE.REVISION);//181
-  geom = await plyToP5geom("../data/room2.ply","room");
+  geom = await plyToP5geom("../data/room.ply","room");
   
   //camera
   cam = createCamera();
@@ -74,6 +82,7 @@ async function setup() {
   let center = eye.copy().add(look);
   cam.setPosition(eye.x,eye.y,eye.z);
   cam.lookAt(center.x,center.y,center.z);
+  cam,perspective(2*atan(height/2/800), width/height, 0.1*800, 10*800)
   
   this.canvas.onwheel = () => false;
   this.canvas.style['touch-action'] = 'none';
@@ -86,18 +95,23 @@ async function setup() {
     this.canvas.onwheel = () => true;
   }
   
+  //バウンディングボックス表示用
+  if(isBoundingBox){
+    bBoxShape = getBoundingBox(boundingBox);
+  }
+  
   //dom
   //カメラ説明
   let guide1 = '🎥カメラの説明を見る';
-  let guide2 = `◼︎マウス🖱️🐭<br>
-                左ドラック視点移動←↑↓→<br>
-				ホイール前後移動▲▼<br>
-				右ドラックカメラ移動◁△▽▷<br>
-                ◼︎タッチ👆<br>
-				1本指視点移動←↑↓→<br>
-				2本指で上下になぞる前後移動▲▼<br>
-				3本指カメラ移動◁△▽▷<br>
-				クリックで閉じる`;
+  let guide2 =  '◼︎マウス🖱️🐭<br>'
+                +'左ドラック視点移動←↑↓→<br>'
+				        +'ホイール前後移動▲▼<br>'
+				        +'右ドラックカメラ移動◁△▽▷<br>'
+                +'◼︎タッチ👆<br>'
+				        +'1本指視点移動←↑↓→<br>'
+				        +'2本指で上下になぞる前後移動▲▼<br>'
+				        +'3本指カメラ移動◁△▽▷<br>'
+				        +'クリックで閉じる';
   camGuide = getMyGUI(guide1,guide2);
   camGuide.btn.position(0,0);
 	camGuide.desc.position(0,0);
@@ -114,19 +128,18 @@ async function setup() {
   memoGUI.btn.style('color','white');
 
   //<p>
-  const p = createP(`工事中⛑️🚧🪏<br>
-                     まだこの部屋までしかありません<br>
-					 今までたくさんの3dジオメトリを<br>
-					 processingで作ってきたので<br>
-					 おもしろい部屋がいっぱいできそうです<br>
-					 お楽しみにദി >⩊<︎︎ ͡ 𐦯<br>
-					 `);
+  const p = createP('工事中⛑️🚧🪏<br>'
+                     +'まだこの部屋までしかありません<br>'
+					           +'今までたくさんの3dジオメトリを<br>'
+					           +'processingで作ってきたので<br>'
+					           +'おもしろい部屋がいっぱいできそうです<br>'
+					           +'お楽しみにദി >⩊<︎︎ ͡ 𐦯<br>'
+					          );
   p.parent(wrapper);
   p.style('width', 'fit-content');
   //p.style('height', 'fit-content');
   //p.style('font-size','20px');
   p.position(width*0.5-p.size().width*0.5,height*0.5-p.size().height*0.5);
-  //p.center();
   p.style('color','white');
 
   //<a>
@@ -143,19 +156,29 @@ function draw() {
  
   if (!ready) {return;}
   
-  background("#C1E2FFFF");
+  //background("#C1E2FFFF");
+  background(80);
 	
   myCamera();
+  //orbitControl();
 	//デバック用
   if(isDebugCam){
     useDebugCam();	
-  } 
-	 
-    push();
-    rotateY(PI/2);
-    rotateX(PI/2)
-    model(geom);
-    pop();
+  }
+  //カメラの領域表示
+  if(isBoundingBox){
+    stroke(0);
+    noFill();
+    model(bBoxShape);
+    fill(255);//適当な数字入れると色、元に戻る
+  }
+
+  push();
+  rotateY(PI/2);
+  rotateX(PI/2)
+  model(geom);
+  scale(3);
+  pop();
 
 	
   //webgl中心(0,0)からどれだけズレているかの相対位置
@@ -164,6 +187,8 @@ function draw() {
                  height/2+ndc.y-memoGUI.btnSize.height*1.5,
                  width/2+ndc.x-memoGUI.descSize.width*0.5,
                  height/2+ndc.y-memoGUI.descSize.height*1.5);
+
+  
 }
 
 async function plyToP5geom(path,id){
@@ -209,63 +234,130 @@ function linearToSRGB(c) {
     : 1.055 * Math.pow(c, 1/2.4) - 0.055;
 }
 
+function getBoundingBox(bBox) {
+  let w = bBox.X_MAX - bBox.X_MIN;
+  let h = bBox.Y_MAX - bBox.Y_MIN;
+  let d = bBox.Z_MAX - bBox.Z_MIN;
+  let x = bBox.X_MIN + w/2;
+  let y = bBox.Y_MIN + h/2;
+  let z = bBox.Z_MIN + d/2;
+	
+  beginGeometry();
+  
+  noFill();
+  stroke(0);
+  strokeWeight(1);
+  push();
+  translate(x,y,z);
+  box(w,h,d);
+  pop();
+  
+  let shape = endGeometry();
+
+  return shape;
+}
+
 function myCamera() {
-    //let x = movedX;//movedX = mouseX - pmouseX.こっちうごかん
+  //let x = movedX;//movedX = mouseX - pmouseX.こっちうごかん
 	//let y = movedY;
-    let x = mouseX - pmouseX;
-    let y = mouseY - pmouseY;
+  let x = mouseX - pmouseX;
+  let y = mouseY - pmouseY;
 	if(mouseIsPressed){
     if(mouseButton === LEFT || touches.length === 1){
-        let dx = x * 0.001;
-	      let dy = y * 0.001;
-		    let vx = config.pan + dx;
-	      let vy = config.tilt + dy;
-		    let target = createVector(cam.centerX,cam.centerY,cam.centerZ);
-		    let pos = createVector(cam.eyeX,cam.eyeY,cam.eyeZ);
-		    let m = p5.Vector.sub(target, pos).mag();
-	      if(config.PAN_MIN < vx && vx < config.PAN_MAX){
-			    config.pan += dx;
-			    //cam.pan(dx);
-				target.add(-tan(dx)*m,0,0);
-				cam.lookAt(target.x,target.y,target.z);
-	      }
-	      if(config.TILT_MIN < vy && vy < config.TILT_MAX){
-		        config.tilt += dy;	
-				//cam.tilt(-dy);
-				target.add(0,-tan(dy)*m,0);
-				cam.lookAt(target.x,target.y,target.z);
-	      }
-    }
-	if(mouseButton === RIGHT || touches.length === 3){
-		let vx = config.moveX + x;
-        let vy = config.moveY + y;
-        if(config.MOVE_X_MIN < vx && vx < config.MOVE_X_MAX){
-			config.moveX += x;
-			cam.move(-x,0,0);
-	    }		
-        if(config.MOVE_Y_MIN < vy && vy < config.MOVE_Y_MAX){
-			config.moveY += y;
-			cam.move(0,-y,0);
-	    }
+      let dx = x * 0.001;
+      let dy = y * 0.001;
+      let vx = config.pan + dx;
+      let vy = config.tilt + dy;
+      let target = createVector(cam.centerX,cam.centerY,cam.centerZ);
+      if(config.PAN_MIN < vx && vx < config.PAN_MAX){
+        config.pan += dx;
+        //cam.pan(dx);
+        target.add(-tan(dx),0,0);
+        cam.lookAt(target.x,target.y,target.z);
       }
+      if(config.TILT_MIN < vy && vy < config.TILT_MAX){
+        config.tilt += dy;	
+        //cam.tilt(-dy);
+        target.add(0,-tan(dy),0);
+        cam.lookAt(target.x,target.y,target.z);
+      }
+    }
+	  if(mouseButton === RIGHT || touches.length === 3){
+      let mvX = getMove(cam,-x,0,0);
+      if(checkBBox(mvX)){
+        cam.move(-x,0,0);
+      }
+      let mvY = getMove(cam,-x,-y,0);
+      if(checkBBox(mvY)){
+        cam.move(0,-y,0);
+      }
+    }
 	  if(touches.length === 2){
-		let vz = config.moveZ + y;
-		if(config.MOVE_Z_MIN < vz && vz < config.MOVE_Z_MAX){
-			config.moveZ += y;
-			cam.move(0,0,y);
-	    }
+      let dy = y * config.sens;
+		   let mv = getMove(cam,0,0,dy);
+      if(checkBBox(mv)){
+        cam.move(0,0,dy);
       }
     }
+  }
 }
 
 function mouseWheel(e) {
-  let vz = config.moveZ + e.delta;
-		if(config.MOVE_Z_MIN < vz && vz < config.MOVE_Z_MAX){
-			config.moveZ += e.delta;
-			cam.move(0,0,e.delta);
-	    }
+  let d = e.delta * config.sens;
+	let mv = getMove(cam,0,0,d);
+  if(checkBBox(mv)){
+    cam.move(0,0,d);
+  }
   //return false; // ページスクロール防止
 }
+
+function checkBBox(v){
+  if(boundingBox.X_MIN < v.x && v.x < boundingBox.X_MAX){
+    if(boundingBox.Y_MIN < v.y && v.y < boundingBox.Y_MAX){
+      if(boundingBox.Z_MIN < v.z && v.z < boundingBox.Z_MAX){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//p5.Camera.js/.move()コピペしてきた
+function getMove(cam,x,y,z) {
+    const local = cam._getLocalAxes();
+	
+    const dx = [local.x[0] * x, local.x[1] * x, local.x[2] * x];
+    const dy = [local.y[0] * y, local.y[1] * y, local.y[2] * y];
+    const dz = [local.z[0] * z, local.z[1] * z, local.z[2] * z];
+    
+	return createVector(cam.eyeX + dx[0] + dy[0] + dz[0],
+                        cam.eyeY + dx[1] + dy[1] + dz[1],
+                        cam.eyeZ + dx[2] + dy[2] + dz[2]);
+}
+//p5.Camera.js
+/*
+move(x, y, z) {
+    const local = this._getLocalAxes();
+
+    // scale local axes by movement amounts
+    // based on http://learnwebgl.brown37.net/07_cameras/camera_linear_motion.html
+    const dx = [local.x[0] * x, local.x[1] * x, local.x[2] * x];
+    const dy = [local.y[0] * y, local.y[1] * y, local.y[2] * y];
+    const dz = [local.z[0] * z, local.z[1] * z, local.z[2] * z];
+
+    this.camera(
+      this.eyeX + dx[0] + dy[0] + dz[0],
+      this.eyeY + dx[1] + dy[1] + dz[1],
+      this.eyeZ + dx[2] + dy[2] + dz[2],
+      this.centerX + dx[0] + dy[0] + dz[0],
+      this.centerY + dx[1] + dy[1] + dz[1],
+      this.centerZ + dx[2] + dy[2] + dz[2],
+      this.upX,
+      this.upY,
+      this.upZ
+    );
+  }
+*/
 
 function setDebugCam() {
   //デバック用
