660a304c0245d91a69fc0068f43d0b37d4ffdc6d
mary-sann <218979812+mary-sann@users.noreply.github.com>
2026-02-21 16:37:42 +0900

    Update roji.js

--- js/roji.js
+++ js/roji.js
@@ -237,37 +237,39 @@ function myCamera() {
   let y = mouseY - pmouseY;
 	if(mouseIsPressed){
     if(mouseButton === LEFT || touches.length === 1){
-        let dx = x * 0.001;
-	      let dy = y * 0.001;
-		    let vx = config.pan + dx;
-	      let vy = config.tilt + dy;
-		    let target = createVector(cam.centerX,cam.centerY,cam.centerZ);
-	      if(config.PAN_MIN < vx && vx < config.PAN_MAX){
-			    config.pan += dx;
-			    //cam.pan(dx);
-				  target.add(-tan(dx),0,0);
-				  cam.lookAt(target.x,target.y,target.z);
-	      }
-	      if(config.TILT_MIN < vy && vy < config.TILT_MAX){
-		      config.tilt += dy;	
-				  //cam.tilt(-dy);
-				  target.add(0,-tan(dy),0);
-				  cam.lookAt(target.x,target.y,target.z);
-	      }
+      let dx = x * 0.001;
+      let dy = y * 0.001;
+      let vx = config.pan + dx;
+      let vy = config.tilt + dy;
+      //let target = createVector(cam.centerX,cam.centerY,cam.centerZ);
+      if(config.PAN_MIN < vx && vx < config.PAN_MAX){
+        config.pan += dx;
+        //cam.pan(dx);
+        let target = createVector(cam.centerX,cam.centerY,cam.centerZ);
+        target.add(-tan(dx),0,0);//tanじゃあ危なすぎる
+        cam.lookAt(target.x,target.y,target.z);
+      }
+      if(config.TILT_MIN < vy && vy < config.TILT_MAX){
+        config.tilt += dy;	
+        //cam.tilt(-dy);
+        let target = createVector(cam.centerX,cam.centerY,cam.centerZ);
+        target.add(0,-tan(dy),0);
+        cam.lookAt(target.x,target.y,target.z);
+      }
     }
 	  if(mouseButton === RIGHT || touches.length === 3){
       let mvX = getMove(cam,-x,0,0);
       if(checkBBox(mvX)){
         cam.move(-x,0,0);
       }
-      let mvY = getMove(cam,0,-y,0);
+      let mvY = getMove(cam,-x,-y,0);
       if(checkBBox(mvY)){
         cam.move(0,-y,0);
       }
     }
 	  if(touches.length === 2){
       let dy = y * config.sens;
-		  let mv = getMove(cam,0,0,dy);
+		   let mv = getMove(cam,0,0,dy);
       if(checkBBox(mv)){
         cam.move(0,0,dy);
       }
